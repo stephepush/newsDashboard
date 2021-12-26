69783638e9a9e2f974652bf3cb056647f6efe3f3
Stephen <7speter@gmail.com>
2021-12-26 00:49:38 -0500

    Gets get request to hackernews api to produce array of relevant top stories data

--- server/app.js
+++ server/app.js
@@ -14,56 +14,30 @@ app.get('/', (req, res) => {
 
 
 app.get('/topStories', async(req, res) => {
-    //console.log(req.params);
-    const api_url = `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`
-    const hn_response = await axios.get(api_url);
-    //const top_story_data = await hn_response;
-    const responseSliced = await hn_response.data.slice(0, 9)
-    const responseLinks = await responseSliced.forEach(id => {
-        axios.get(` https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
-            .then((response) => {
-                const stories = []
-                stories.push(response.data)
-                console.log(stories)
-                return stories
-            }).catch((err) => {
-                this.err = err
+            try {
+                const api_url = `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`
+                const hn_response = await axios.get(api_url);
+                //const top_story_data = await hn_response;
+                const responseSliced = await hn_response.data.slice(0, 9)
+                console.log(responseSliced)
+                const responseLinks = await responseSliced.map(id => {
+                    return `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+                })
+                console.log(responseLinks)
+                const response = await axios.all(responseLinks.map(link => axios.get(link)))
+                console.log(response) //receive massive streams for each item returned
+                const topStories = response.map(item => item.data)
+                console.log(topStories)
+            } catch (error) {
+                console.error(error)
+            }
+
+
+            //})
+
+            //the id's are being formatted into the item endpoint format on line 31... pass the array of those links
+            //back to the api to get the relevant info, then pass that to the frontend
+
+            app.listen(port, () => {
+                console.log(`Running app on port ${port}`)
             })
-    })
-    console.log(responseLinks)
-        /* const data = {
-            content: hn_response
-        } */
-        //console.log(hn_response)
-        /*try {
-
-
-            const responseSliced = hn_response.data.slice(0, 9)
-
-            const responseLinks = responseSliced.forEach(id => {
-
-                axios.get(` https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`)
-                    .then((response) => {
-                        const stories = []
-                        stories.push(response.data)
-                            //console.log(stories)
-                        return stories
-                    }).catch((err) => {
-                        this.err = err
-                    })
-            })
-            console.log(stories)
-            console.log(responseLinks)
-            return responseLinks
-        } catch (error) {
-            console.error(error)
-        }*/
-
-})
-
-//the id's are being formatted into the item endpoint format on line 31... pass the array of those links
-//back to the api to get the relevant info, then pass that to the frontend
-
-app.listen(port, () => {
-    console.log(`Running app on port ${port}`)
-})
