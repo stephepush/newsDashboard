064f1c0d2da4fea7daf17c2a253d541fc46185bb
Stephen <7speter@gmail.com>
2021-12-26 15:21:13 -0500

    Revises/improves api calls

--- server/app.js
+++ server/app.js
@@ -14,30 +14,30 @@ app.get('/', (req, res) => {
 
 
 app.get('/topStories', async(req, res) => {
-            try {
-                const api_url = `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`
-                const hn_response = await axios.get(api_url);
-                //const top_story_data = await hn_response;
-                const responseSliced = await hn_response.data.slice(0, 9)
-                console.log(responseSliced)
-                const responseLinks = await responseSliced.map(id => {
-                    return `https://hacker-news.firebaseio.com/v0/item/${id}.json`
-                })
-                console.log(responseLinks)
-                const response = await axios.all(responseLinks.map(link => axios.get(link)))
-                console.log(response) //receive massive streams for each item returned
-                const topStories = response.map(item => item.data)
-                console.log(topStories)
-            } catch (error) {
-                console.error(error)
-            }
+    try {
+        const api_url = `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&orderBy="$key"&limitToFirst=10`
+        const hn_response = await axios.get(api_url);
+        //const top_story_data = await hn_response;
+        //const responseSliced = await hn_response.data.slice(0, 9)
+        //console.log(responseSliced)
+        const responseLinks = await hn_response.data.map(id => {
+            return `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+        })
+        console.log(responseLinks)
+        const res = await axios.all(responseLinks.map(link => axios.get(link)))
+        const topStories = res.map(item => item.data)
+        console.log(topStories)
+    } catch (error) {
+        console.error(error)
+    }
+});
 
 
-            //})
+//})
 
-            //the id's are being formatted into the item endpoint format on line 31... pass the array of those links
-            //back to the api to get the relevant info, then pass that to the frontend
+//the id's are being formatted into the item endpoint format on line 31... pass the array of those links
+//back to the api to get the relevant info, then pass that to the frontend
 
-            app.listen(port, () => {
-                console.log(`Running app on port ${port}`)
-            })
+app.listen(port, () => {
+    console.log(`Running app on port ${port}`)
+})
