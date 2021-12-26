f1c8576910f61c974166ff79ebba4467eb1e692e
Stephen <7speter@gmail.com>
2021-12-26 17:38:25 -0500

    Adds app.use statement and sends apiCall.js to client

--- server/app.js
+++ server/app.js
@@ -3,17 +3,19 @@ const app = express();
 const path = require('path');
 const axios = require('axios');
 const port = 3000
-
+app.use(express.static('public'));
+//app.use(express.json({ limit: '1mb' }));
 /* app.get('/', (req, res) => {
     res.send('Hello World!')
 }) */
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'))
+    res.sendFile(path.join(__dirname + '/public/apiCall.js'))
 })
 
 
-app.get('/topStories', async(req, res) => {
+app.get('/topStories', async(request, response) => {
     try {
         const api_url = `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&orderBy="$key"&limitToFirst=10`
         const hn_response = await axios.get(api_url);
@@ -24,12 +26,14 @@ app.get('/topStories', async(req, res) => {
             return `https://hacker-news.firebaseio.com/v0/item/${id}.json`
         })
         console.log(responseLinks)
-        const res = await axios.all(responseLinks.map(link => axios.get(link)))
-        const topStories = res.map(item => item.data)
-        console.log(topStories)
+        const api_res = await axios.all(responseLinks.map(link => axios.get(link)))
+        const topStories = await api_res.map(item => item.data)
+            //console.log(topStories)
+        response.json(topStories)
     } catch (error) {
         console.error(error)
     }
+
 });
 
 
