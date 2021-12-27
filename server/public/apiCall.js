33eec3aa167c368c5412d0543a142fd7224b65e6
Stephen <7speter@gmail.com>
2021-12-27 00:00:36 -0500

    Adds code to html and public js script to get hn news items to render on client

--- server/public/apiCall.js
+++ server/public/apiCall.js
@@ -1,10 +1,43 @@
 async function call() {
-    const api_url = `/topStories`
-    const response = await fetch(api_url)
-    const json = await response.json()
-    console.log(json)
+    const ul = document.getElementById('posts')
+    const list = document.createDocumentFragment()
+    try {
 
-    json.forEach(item => item)
+
+        const api_url = `/topStories`
+        const response = await fetch(api_url)
+        const json = await response.json()
+        console.log(json)
+
+        await json.forEach(post => {
+            let li = document.createElement('li')
+            let headline = document.createElement('h2')
+            let poster = document.createElement('h3')
+            let comments = document.createElement('p')
+            let score = document.createElement('p')
+
+
+
+            headline.innerText = `${post.title}`
+            poster.innerText = `Posted by ${post.by}`
+            comments.innerText = `${post.descendants} comments`
+            score.innerText = `${post.score} points`
+
+            li.appendChild(headline)
+            li.appendChild(poster)
+            li.appendChild(comments)
+            li.appendChild(score)
+            li.classList.add("list-group-item")
+            list.appendChild(li)
+        })
+    } catch {
+        console.error(error)
+    } finally {
+        ul.appendChild(list)
+    }
+
+
+
 }
 
 
